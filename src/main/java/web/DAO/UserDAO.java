744f0de1618bfe3db68a216e311d9d05acee4b5a
Nikodim-monk <lr1975@yandex.ru>
2022-08-12 17:38:49 +0400

    псевдо КРУД с hiber&Sql вар2

---
+++ src/main/java/web/DAO/UserDAO.java
@@ -0,0 +1,44 @@
+package web.DAO;
+
+import web.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    private List<User> users = new ArrayList<>();
+
+    {
+        users.add(new User(1l, "Андрей", (byte) 46));
+        users.add(new User(2l, "Иван", (byte) 50));
+        users.add(new User(3l, "Сидор", (byte) 54));
+        users.add(new User(4l, "Michael", (byte) 30));
+        users.add(new User(5l, "John", (byte) 36));
+    }
+
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+    public User getUserById(int id) {
+        return users.get(id - 1);
+    }
+
+    public void addNewUser(User user) {
+        user.setId((long) users.size()+1);
+        users.add(user);
+    }
+
+    public void updateUser(User user, int id) {
+        User userNotUpdate = getUserById(id);
+        userNotUpdate.setName(user.getName());
+        userNotUpdate.setAge(user.getAge());
+    }
+
+    public void UserDelete(int id) {
+        users.remove(id - 1);
+        for (int i = 0; i < users.size(); i++) {
+            users.get(i).setId((long) (i+1));
+        }
+    }
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,49 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UserID")
+    private Long Id;
+    @Column(name = "UserName")
+    private String Name;
+    @Column(name = "UserAge")
+    private byte Age;
+
+    public User() {
+    }
+
+    public User(Long id, String name, byte age) {
+        this.Id = id;
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        this.Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public byte getAge() {
+        return Age;
+    }
+
+    public void setAge(byte age) {
+        this.Age = age;
+    }
+}
